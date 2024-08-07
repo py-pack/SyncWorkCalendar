0f26ef30e09aa44a3854df2f576b9c4552992437
ymnik <v.s@dcjudgement.com>
2024-08-07 23:27:58 +0200

    #2 Create project

---
+++ src/config.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 5432
+    database: str = "db"
+    user: str = "user"
+    password: str = "pass"
+
+    echo: bool = (False,)
+    echo_pool: bool = (False,)
+    pool_size: int = (5,)
+    max_overflow: int = (10,)
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+    @property
+    def url(self) -> PostgresDsn:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),  # порядок підвантаження мержа змінинних
+        case_sensitive=False,  # не важливий регістр
+        env_nested_delimiter="__",  # розділювач
+        env_prefix="APP__",  # префікс змінних, які будуть автоматично парситись
+        env_ignore_empty=True,  # ігнорувати пусті значення
+    )
+    db: DatabaseConfig = DatabaseConfig()
+
+
+settings = Settings()

380423678eb6a50db00782563f8d5700b377d0a7
ymnik <v.s@dcjudgement.com>
2024-09-01 19:38:31 +0200

    #3 refactoring db session

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,9 @@
 __all__ = (
-    "db_helper"
+    "db_helper",
+    "sync_connection",
+    "sync_sessin",
 )
 
 from .db_helper import db_helper
+from .db_helper import sync_connection
+from .db_helper import sync_sessin
--- src/core/db_helper.py
+++ src/core/db_helper.py
@@ -1,6 +1,8 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from src.config import settings
 
@@ -14,7 +16,7 @@ class DatabaseHelper:
         pool_size: int = 5,  # кількість відкрити з'єднань
         max_overflow: int = 10,  # кількість підключень в пулі
     ):
-        self.engine = create_async_engine(
+        self.engine_async = create_async_engine(
             url=url,
             echo=echo,
             echo_pool=echo_pool,
@@ -23,14 +25,15 @@ class DatabaseHelper:
         )
 
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
-            bind=self.engine,
+            bind=self.engine_async,
+            class_=AsyncSession,
             autocommit=False,
             autoflush=False,
             expire_on_commit=False,
         )
 
     async def dispose(self) -> None:
-        await self.engine.dispose()
+        await self.engine_async.dispose()
 
     async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
@@ -44,3 +47,13 @@ db_helper = DatabaseHelper(
     pool_size=int(settings.db.pool_size),
     max_overflow=settings.db.max_overflow,
 )
+
+
+def sync_connection():
+    engine = create_engine(settings.db.url_sync)
+    return engine.connect()
+
+
+def sync_sessin():
+    engine = create_engine(settings.db.url_sync)
+    return Session(engine)
